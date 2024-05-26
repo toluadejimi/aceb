99f0e50515d3e2760fc971be10138626376da212
Jimmy <toluadejimi@gmail.com>
2024-05-26 14:41:44 +0100

    bot added

--- core/app/Http/Controllers/User/ApiController.php
+++ core/app/Http/Controllers/User/ApiController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
@@ -20,5 +23,36 @@ class ApiController extends Controller
         $user->save();
         $notify[] = ['success', 'Generated new api key!'];
         return back()->withNotify($notify);
+    }
+
+
+    public function e_fund(request $request)
+    {
+
+        $get_user = User::where('email', $request->email)->first() ?? null;
+
+        if ($get_user == null) {
+
+            return response()->json([
+                'status' => false,
+                'message' => 'No one user found, please check email and try again',
+            ]);
+        }
+
+
+            User::where('email', $request->email)->increment('balance', $request->amount) ?? null;
+
+
+        $amount = number_format($request->amount, 2);
+
+        Transaction::where('ref_id', $request->order_id)->update(['status' => 2]);
+
+
+        return response()->json([
+            'status' => true,
+            'message' => "NGN $amount has been successfully added to your wallet",
+        ]);
+
+
     }
 }
